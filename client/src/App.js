7286400c2bde5575180a75753fb40dd467d9cac7
Jean-Marc Robin <jmrobin2pi@gmail.com>
2019-07-15 07:34:45 +0200

    Modify Other Page

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
           <header className="App-header">
             <Link to="/">Home</Link>
             {" > "}
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/otherpage">Some Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
